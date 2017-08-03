856eee96676e742c7608a4abfb400ee626e3ef3b
Breandon Smith <brandon@reddirtwebdesign.com>
2017-08-03 05:27:17 -0400

    Working Ny Times API Call
    	-Get JSON for NYTIMES is working
    	-Background image using google street view is not working

--- portfolio/minicourse-ajax-project/js/script.js
+++ portfolio/minicourse-ajax-project/js/script.js
@@ -13,9 +13,35 @@ function loadData() {
 
     // load streetview
 
-    // YOUR CODE GOES HERE!
+    var streetStr = $("#street").val();
+    var cityStr = $("#city").val();
+    var address = streetStr + ', ' + cityStr;
+  
+    $greeting.text('So, you want to live at ' + address + '?');
+  
+    var streetUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '';
+  
+    $body.append('<img class="bgimg" src="' + streetUrl + '">');
+    
+   //Nytimes 
+  
+  var nyTimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=2afb075c949a4f8f982a8395a7bc6d50'
+
+  $.getJSON(nyTimesUrl, function(data){
+    
+    $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+    
+    var articles = data.response.docs;
+    for(var i = 0; i < 10; i++) {
+      var article = articles[i];
+      $nytElem.append('<li class="article">' + '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                     '<p>' + article.snippet + '</p>' + 
+                     '</li>');
+    }
+    console.log(data);
+  })
 
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
