74edb39ccfe248bb1a4959769f9bfebfc544c2ff
Brandon Smith <Brandon@reddirtwebdesign.com>
2017-08-08 04:24:13 -0700

    Markers Not Working

--- portfolio/Projects/Neighborhood Map/js/app.js
+++ portfolio/Projects/Neighborhood Map/js/app.js
@@ -1,11 +1,13 @@
+//View Model
+
 var viewModel = function() {
     var self = this;
 
     //Variable used to toggle the slide out menu
-    self.showTheMenu = ko.observable(true);
+    self.showTheMenu = ko.observable(false);
     
     //variables to store the marker locations as well as the li's in the slide out menu
-    self.markers = ko.observableArray ([
+    self.places = ko.observableArray ([
     	{title: 'Elementary School', location: {lat: 33.443027, lng: -112.407235}},
     	{title: 'High School', location: {lat: 33.437227, lng: -112.398954}},
     	{title: 'Frys Marketplace', location: {lat: 33.437713, lng: -112.411278}},
@@ -17,43 +19,32 @@ var viewModel = function() {
     self.showMenu = function() {
         self.showTheMenu(!self.showTheMenu());
     }
+		//Initialize The Map
+		 //Place Markers On The Map
 
-};
+	 
+			
+}
 
     
 ko.applyBindings(new viewModel());
 
-//Initialize The Map
-function initMap() {
-	 var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 33.450121, lng: -112.401882 },
-		zoom: 15,
-		disableDefaultUI: true
-	});
-	 //Place Markers On The Map
-	 var marker1 = new google.maps.Marker({
-	 	position: {lat: 33.437227, lng: -112.398954},
-	 	map: map,
-	 	title: "High School"
-	 });
-	 var marker2 = new google.maps.Marker({
-	 	position: {lat: 33.437713, lng: -112.411278},
-	 	map: map,
-	 	title: "Fry's Marketplace"
-	 });
-	 var marker3 = new google.maps.Marker({
-	 	position: {lat: 33.443027, lng: -112.407235},
-	 	map: map,
-	 	title: "Elementary School"
-	 });
-	 var marker4 = new google.maps.Marker({
-	 	position: {lat: 33.453093, lng: -112.391905},
-	 	map: map,
-	 	title: "Quick Trip"
-	 });
-	 var marker5 = new google.maps.Marker({
-	 	position: {lat: 33.459454, lng: -112.394149},
-	 	map: map,
-	 	title: "Walmart"
-	 });
-}
+//Model
+	 function initMap() {
+		 var map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: 33.450121, lng: -112.401882 },
+			zoom: 15,
+			disableDefaultUI: true
+		});
+	};
+	createMakers = function() {
+	 for (var i = 0; i < places.title.length;i++) {
+	    var place = viewModel.places[i];
+	    var location = viewModelplace[i].location;
+	    var marker1 = new google.maps.Marker({
+	        map: map,
+	        position: location
+	    	});
+		}
+	}
+
